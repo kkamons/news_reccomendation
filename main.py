9898886bb0a2984de1333baa02840c754b6abfad
Keith <keith@kamons.com>
2020-11-18 20:33:17 -0600

    progress

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from pathlib import Path, PureWindowsPath
 
 num_trials = 10
+num_readers=1
 lean_map = {-1:"left", -0.5:"lean left", 0:"center", 0.5:"lean right", 1:"right"}
 def gen_reader_sample(n, com):
 
@@ -47,7 +48,9 @@ def calc_prob_reading(reader_leaning, reader_trust, source_leaning):
 	#If the farther the media outlet sits from the viewer_leaning, the less likely they will be to read
 	# prob = np.random.normal(reader_trust- abs(reader_leaning-source_leaning), variance,1)[0]
 	noise = np.random.normal(0,variance,1)[0]
+	print(reader_trust)
 	prob = (reader_trust - abs(reader_leaning-source_leaning) + noise)#/(reader_trust + abs(reader_leaning-source_leaning) + abs(noise))
+	print(prob)
 	if prob < 0:
 		prob = 0
 	if prob > 1:
@@ -74,20 +77,30 @@ def recalc_trust(reader_trusts_story, reader_trust):
 	return new_reader_trust
 
 def recalc_leaning(reader_leaning, new_reader_trust,source_leaning):
-	return source_leaning*new_reader_trust
+	# if (reader_leaning - source_leaning) <=0.5:
+	# 	return reader_leaning + np.sign(reader_leaning)*0.05
+	# elif (reader_leaning - source_leaning) < 0.1:
+	# 	return reader_leaning + np.sign(reader_leaning)*0.1
+	# else:
+	# 	return reader_leaning
+	return reader_leaning + np.sign(reader_leaning)*0.03
+
+	
+	# return (reader_leaning - source_leaning)
+	# return source_leaning*new_reader_trust
 def main():
-	mac=True
+	mac=False
 # Big sad the path shit dont work but its fine
 	if(mac):
 		demo_path = Path("projdata//Voter_Distribution_2016//Voter_demo_analysis_CSV.csv")
 		all_path = Path("projdata//Sources_Political_Leanings//all.csv")
 		trust_path= Path("projdata//Trust_In_Media.csv")
 	else:
-		print("Windows is dumb")
 		demo_path = Path("projdata\\Voter_Distribution_2016\\Voter_demo_analysis_CSV.csv")
 		all_path = Path("projdata\\Sources_Political_Leanings\\all.csv")
 		trust_path= Path("projdata\\Trust_In_Media.csv")
-	n=10
+	
+	
 
 	demos = pd.read_csv(demo_path, encoding='unicode_escape')
 	sources = pd.read_csv(all_path, encoding='unicode_escape')
@@ -98,21 +111,27 @@ def main():
 	media_frame = media_trust.merge(src_bias_df, on="source")
 	# print(media_frame)
 	#This will generate a sample of readers
-	reader_df = gen_reader_sample(n, media_frame)
+	reader_df = gen_reader_sample(num_readers, media_frame)
 	# print(reader_df.head())
 	# reader_df.loc[reader_df.leaning == -1, src]
 	# says: "select src from reader_df where leaning = -1"
 	readers = reader_df["reader_id"].unique()
+	# print('+++++++++++++++++ ', reader_df.head())
 	# print(reader_df)
+	leaning_prog = []
 	for i in range(num_trials):
+		print('________________________________________New Trial__________________________________________________')
+		print(reader_df.leaning.values)
+		leaning_prog.append(list(reader_df.leaning.values))
 		for reader in readers:
+			print("============================ new reader ======================================")
 		# Get the source to recommend to the reader
 		# reader = 0.0
 			rec_source = getSource(media_frame)
 			print("Source: ",rec_source)
 			# Calc prob of reader reading source
 			reader_leaning = reader_df.leaning[reader]
-			print("Reader Leaning:",reader_leaning, lean_map[reader_leaning])
+			print("Reader Leaning:",reader_leaning)#, lean_map[reader_leaning])
 			# reader_trust = media_frame.loc[media_frame.source == rec_source, lean_map[reader_leaning]].values[0]
 			reader_trust = reader_df.loc[reader_df.reader_id == reader, rec_source].values[0]
 
@@ -134,8 +153,8 @@ def main():
 				new_trust_level = recalc_trust(reader_trusts_story, reader_trust)
 				reader_df.loc[reader_df.reader_id == reader, rec_source] = new_trust_level
 				#reCalcPolLeaning - im thinking this should be harder to change than the trust level in the media outlet
-				reader_df.loc[reader_df.reader_id == reader, "bias"] = recalc_leaning(reader_leaning, new_trust_level,source_leaning)
-				print("New Leaning: ", reader_df.loc[reader_df.reader_id == reader, "bias"].values[0])
+				reader_df.loc[reader_df.reader_id == reader, "leaning"] = recalc_leaning(reader_leaning, new_trust_level,source_leaning)
+				print("New Leaning: ", reader_df.loc[reader_df.reader_id == reader, "leaning"].values[0])
 				
 			else:
 				print("Fake news!")
@@ -154,6 +173,7 @@ def main():
 	# print(percent_lean_right)
 	# print(percent_right)
 	# print(reader_df[reader_df.bias > 0.5].bias.values)
+	print(leaning_prog)
 	
 # TODO:
 # X CalcProbOfReading()
